f7a858b6b3a4c6e105601c5ca81e6af81891bdf8
Jason Ruiz <burgersbacon@Jasons-MacBook-Pro.local>
2019-11-05 00:24:06 -0500

    now the bot can search reddit posts and tweet them, not automatically

--- app.js
+++ app.js
@@ -1,17 +1,64 @@
 const config = require('./config');
 const Twit = require('twit');
-const Twitter = new Twit(config);
+const snoowrap = require('snoowrap');
+const Twitter = new Twit(config.mainTwitter);
+const TwitterLinks = new Twit(config.sourceTwitter);
+const Reddit = new snoowrap(config.reddit);
+const http = require('http');
+const fs = require('fs');
 
-let params = {
+const postTweet = (params, redditPost) => {
+  Twitter.post('statuses/update', params, function(err, data, response) {
+    console.log(data, data.id, redditPost);
 
-}
+    let paramsForReply = {
+      in_reply_to_status_id: data.id,
+      status: `@${data.user.screen_name} stolen from: https://reddit.com${redditPost.permalink}, posted (probably stolen too) by: /u/${redditPost.author.name}, via Reddit`
+    }
 
-function postTweet() {
-  console.log("executing")
-  Twitter.post('statuses/update', {status: 'i did not decided to live, but here i am... i mean, bip bop bop bip'}, function(err, data, response) {
-    console.log(data);
-    console.log("data");
+    TwitterLinks.post('statuses/update', paramsForReply);
   })
 }
 
-postTweet();
+const getUpvotedPosts = () => {
+  Reddit._get({uri: 'user/l33t_supa_h4x0r_/upvoted/'}).then(posts => {
+    var httpLink = posts[0].url.replace("https", "http");
+    downloadMedia(httpLink, `images/${posts[0].id}`, (urlMedia) => {
+      postPhoto(urlMedia, posts[0]);
+    });
+  });
+}
+
+const downloadMedia = (url, dest, cb) => {
+  let file = fs.createWriteStream(dest);
+  let request = http.get(url, function(response) {
+    response.pipe(file);
+    file.on('finish', function() {
+      file.close(cb(dest));
+    });
+  });
+}
+
+const postPhoto = (urlImage, redditPost) => {
+
+  var b64content = fs.readFileSync(urlImage, { encoding: 'base64' });
+
+  Twitter.post('media/upload', { media_data: b64content }, function (err, data, response) {
+    var mediaIdStr = data.media_id_string
+    var meta_params = { media_id: mediaIdStr}
+
+    Twitter.post('media/metadata/create', meta_params, function (err, data, response) {
+      if (!err) {
+        var params = { status: '', media_ids: [mediaIdStr] }
+        postTweet(params, redditPost);
+      }
+    })
+  });
+}
+
+getUpvotedPosts();
+
+
+//getUpvotedPosts();
+
+//postTweet();
--- config.js
+++ config.js
@@ -1,6 +1,20 @@
 module.exports = {
-  consumer_key: 'XXXXXXXXXXXXXXXXXXX',
-  consumer_secret: 'XXXXXXXXXXXXXX',
-  access_token: 'XXXXXXXXX',
-  access_token_secret: 'XXXTENTACION'
+  mainTwitter: {
+    consumer_key: 'XXXXXXXXXXXXXX',
+    consumer_secret: 'XXXXXXXXXXXXXX',
+    access_token: 'XXXXXXXXXXXXXX',
+    access_token_secret: 'XXXXXXXXXXXXXX'
+  },
+  reddit: {
+    userAgent: 'XXXXXXXXXXXXXX',
+    clientId: 'XXXXXXXXXXXXXX',
+    clientSecret: 'XXXXXXXXXXXXXX',
+    refreshToken: 'XXXXXXXXXXXXXX'
+  },
+  sourceTwitter: {
+    consumer_key: 'XXXXXXXXXXXXXX',
+    consumer_secret: 'XXXXXXXXXXXXXX',
+    access_token: 'XXXXXXXXXXXXXX',
+    access_token_secret: 'XXXXXXXXXXXXXX'
+  }
 }
