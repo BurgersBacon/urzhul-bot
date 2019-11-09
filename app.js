774abbfb5f7dce706c8b32ebdfacedfd0f3568ba
Jason Ruiz <burgersbacon@Jasons-MacBook-Pro.local>
2019-11-08 23:29:21 -0500

    now the bot can use the database and post tweets

--- app.js
+++ app.js
@@ -7,27 +7,97 @@ const Reddit = new snoowrap(config.reddit);
 const http = require('http');
 const fs = require('fs');
 
-const postTweet = (params, redditPost) => {
-  Twitter.post('statuses/update', params, function(err, data, response) {
-    console.log(data, data.id, redditPost);
+const MongoClient = require('mongodb').MongoClient;
+const uri = "mongodb+srv://burgersbacon:ayylmao@burgersbacon-lvg3y.mongodb.net/urzhul-bot"
+const client = new MongoClient(uri, { useNewUrlParser: true });
 
-    let paramsForReply = {
-      in_reply_to_status_id: data.id,
-      status: `@${data.user.screen_name} stolen from: https://reddit.com${redditPost.permalink}, posted (probably stolen too) by: /u/${redditPost.author.name}, via Reddit`
-    }
 
-    TwitterLinks.post('statuses/update', paramsForReply);
+client.connect(err => {
+  const db = client.db("urzhul-bot");
+
+  setInterval(db => {
+    getUpvotedPosts(db);
+    postMedia(db);
+  }, 60000, db)
+});
+
+
+
+const postMedia = (db) => {
+  db.collection("pendingPosts").find().sort({id: -1}).limit(1).toArray().then(posts => {
+    var httpLink = posts[0].url.replace("https", "http");
+    downloadMedia(httpLink, `images/${posts[0].id}`, (urlMedia) => {
+      postPhoto(urlMedia, posts[0], db);
+    });
   })
 }
 
-const getUpvotedPosts = () => {
-  Reddit._get({uri: 'user/l33t_supa_h4x0r_/upvoted/'}).then(posts => {
-    var httpLink = posts[0].url.replace("https", "http");
-    downloadMedia(httpLink, `images/${posts[0].id}`, (urlMedia) => {
-      postPhoto(urlMedia, posts[0]);
+
+const getUpvotedPosts = (db) => {
+  Reddit._get({uri: 'user/l33t_supa_h4x0r_/upvoted/'}).then(redditPosts => {
+
+    let newPosts = [];
+
+    db.collection("pendingPosts").find().toArray().then(pendingPosts => {
+      redditPosts.forEach(redditPost => {
+
+          let found = pendingPosts.find(pendingPost => {
+            return pendingPost.id == redditPost.id;
+          });
+          if (!found) {
+            console.log("FOUND")
+            db.collection("postedPosts").findOne({
+              id: redditPost.id
+            }).then(lmao => {
+              if (!lmao) {
+                db.collection("pendingPosts").insertOne({
+                  id: redditPost.id,
+                  url: redditPost.url,
+                  permalink: redditPost.permalink,
+                  author: redditPost.author.name
+                })
+                .then(() => {
+                  console.log("added to database")
+                })
+                .catch(err => {
+                  if (err) console.log("AHHHHHHAHAHAHHH")
+                });
+              } else {
+                console.log("ya esta posteado shavo")
+              }
+            });
+          }
+        });
     });
   });
 }
+
+const postTweet = (params, redditPost, db) => {
+  Twitter.post('statuses/update', params, function(err, data, response) {
+    let paramsForReply = {
+      in_reply_to_status_id: data.id_str,
+      status: `@${data.user.screen_name} stolen from: https://reddit.com${redditPost.permalink}, posted (probably stolen too) by: /u/${redditPost.author}, via Reddit`
+    }
+
+    TwitterLinks.post('statuses/update', paramsForReply, function(err, data, response) {
+      db.collection('pendingPosts').deleteMany({
+        id: redditPost.id
+      }).then(() => {
+        console.log(redditPost.id)
+        db.collection("postedPosts").insertOne({
+          id: redditPost.id,
+          url: redditPost.url,
+          permalink: redditPost.permalink,
+          author: redditPost.author
+        }).then(() => {
+          console.log("-------")
+        });
+      })
+    });
+  })
+}
+
+
 
 const downloadMedia = (url, dest, cb) => {
   let file = fs.createWriteStream(dest);
@@ -39,7 +109,7 @@ const downloadMedia = (url, dest, cb) => {
   });
 }
 
-const postPhoto = (urlImage, redditPost) => {
+const postPhoto = (urlImage, redditPost, db) => {
 
   var b64content = fs.readFileSync(urlImage, { encoding: 'base64' });
 
@@ -50,15 +120,8 @@ const postPhoto = (urlImage, redditPost) => {
     Twitter.post('media/metadata/create', meta_params, function (err, data, response) {
       if (!err) {
         var params = { status: '', media_ids: [mediaIdStr] }
-        postTweet(params, redditPost);
+        postTweet(params, redditPost, db);
       }
     })
   });
 }
-
-getUpvotedPosts();
-
-
-//getUpvotedPosts();
-
-//postTweet();
